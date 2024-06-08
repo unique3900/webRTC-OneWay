364d1a68a60922abad0f575a29239dd7070decfa
unique3900 <parasharneupane3900@gmail.com>
2024-06-08 10:14:41 +0545

    Backend Completed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,44 @@
-import { WebSocketServer } from "ws";
+import { WebSocket, WebSocketServer } from "ws";
 
-const wss=new WebSocketServer({port:8080});
+const wss = new WebSocketServer({ port: 8080 });
 
+let senderSocket: WebSocket | null = null;
+let receiverSocket: WebSocket | null = null;
 
-wss.on("connection",(socket)=>{
-    console.log("Connected")
-    socket.on("error",(err)=>{
-        console.log(err)
-    })
-    socket.on("message",(data)=>{
-        console.log({...data})
-    })
-})
+wss.on("connection", (socket) => {
+  socket.on("message", (data: any) => {
+    const message = JSON.parse(data);
+
+    if(message.type==="identify-sender"){
+        senderSocket=socket;
+    }
+    else if(message.type==="identify-receiver"){
+        receiverSocket=socket;
+    }
+    else if(message.type==="create-offer"){
+        if(socket!==senderSocket) return;
+        senderSocket?.send(JSON.stringify({
+            type:"create-answer",
+            sdp:message.sdp
+        }))
+    }
+    else if(message.type==="ice-candidate"){
+        if(socket===senderSocket){
+            receiverSocket?.send(JSON.stringify({
+                type:"ice-candidate",
+                candidate:message?.candidate
+            }))
+        }
+        else if(socket===receiverSocket){
+            senderSocket?.send(JSON.stringify({
+                type:"ice-candidate",
+                candidate:message?.candidate
+            }))
+        }
+    }
+  });
+  
+  socket.on("error", (err) => {
+    console.log("Something went wrong", err);
+  });
+});
